9d96facd87b3138a599b8dd89e1ac9a0da396a9b
Sinthorn Pradutnam <psinthorn@gmail.com>
2018-12-08 21:46:14 +0700

    Updates

---
+++ precache-manifest.4c2116d07323d1c067bbea2279fda0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "a4771ca5d8338e485067",
+    "url": "/contactmanager/static/js/main.a4771ca5.chunk.js"
+  },
+  {
+    "revision": "16eec99c72ca577b52a8",
+    "url": "/contactmanager/static/js/1.16eec99c.chunk.js"
+  },
+  {
+    "revision": "a4771ca5d8338e485067",
+    "url": "/contactmanager/static/css/main.dde30680.chunk.css"
+  },
+  {
+    "revision": "16eec99c72ca577b52a8",
+    "url": "/contactmanager/static/css/1.67506555.chunk.css"
+  },
+  {
+    "revision": "bf8aead673efa919b6f56f80fab45c40",
+    "url": "/contactmanager/index.html"
+  }
+];
